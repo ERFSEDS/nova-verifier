887f47ac404aad8e3c5896ccdd88085ebcc8e0e6
Troy Neubauer <troyneubauer@gmail.com>
2022-03-10 07:58:54 -0500

    Remove python wrapper code and change name to nova-verifier

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,6 @@
 pub mod lower;
 pub mod upper;
 
-use pyo3::create_exception;
-use pyo3::exceptions::PyException;
-use pyo3::prelude::*;
-use pyo3::{iter::IterNextOutput, prelude::*, PyIterProtocol};
-
-create_exception!(verifier, TomlParseException, PyException);
-create_exception!(verifier, VerifyException, PyException);
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum CommandError {
     NoValues,
@@ -54,25 +46,8 @@ pub enum Error {
     NoStates,
 }
 
-fn verify_inner(toml: &str) -> Result<Vec<u8>, Error> {
+pub fn verify(toml: &str) -> Result<Vec<u8>, Error> {
     let mid = upper::verify(toml)?;
     let lower = lower::verify(mid)?;
     Ok(postcard::to_stdvec(&lower)?)
-}
-
-#[pyfunction]
-fn verify(toml: String) -> PyResult<Vec<u8>> {
-    let result = verify_inner(toml.as_str());
-    result.map_err(|e| TomlParseException::new_err(format!("{}", e)))
-}
-
-#[pymodule]
-fn verifier(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add("TomlParseException", py.get_type::<TomlParseException>())?;
-
-    m.add("VerifyException", py.get_type::<VerifyException>())?;
-
-    m.add_function(wrap_pyfunction!(verify, m)?)?;
-
-    Ok(())
 }
